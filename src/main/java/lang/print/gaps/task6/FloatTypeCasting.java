7ddfcc69dc77ca397a50f6f93ff751834cb774fd
Aleke <alnur.rysbek@mail.ru>
2022-10-31 09:47:42 +0600

    success

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.task6;
 
+import java.math.RoundingMode;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        System.out.println(Math.round(numberToBeRounded));
     }
 }
